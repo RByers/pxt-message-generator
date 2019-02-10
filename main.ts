97c041e12593aa47109ac9bab0853325c9040e1a
Rick Byers <47506166+RByers-makecode@users.noreply.github.com>
2019-02-10 15:05:39 -0500

    Test commit

--- main.ts
+++ main.ts
@@ -1,7 +1,31 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+
+/* function showChar() {
+    if (idx < 0) {
+        basic.showIcon(IconNames.Yes);
+    } else {
+        let ch = chars.charAt(idx);
+        basic.showString(ch, 0);
+    }
+} */
+
+const TILT_X_THRESHOLD = 100; // in micro-gs
+const TILT_X_SPEED = 10;      // chars/second/g
+
+function getMessage() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ .!";
+    let idx = 0;
+    let message = "";
+    let lastChange = 0;
+
+    while (true) {
+        let accX = input.acceleration(Dimension.X);
+        if (Math.abs(accX) >= TILT_X_THRESHOLD) {
+
+        }
+        basic.pause(20);
+    }
+    return message;
+}
+
+let msg = getMessage();
+basic.showString(msg);
